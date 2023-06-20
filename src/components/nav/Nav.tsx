0750c4bb3aa05bcf547c759196f33cc5c4d5eb90
tjwldnjs123 <tjwldnjs12345@naver.com>
2023-06-20 17:19:14 +0900

    [modify]: nav

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -8,7 +8,7 @@ function Nav() {
 
   return (
     <div className="z-20 fixed sm:top-4 sm:right-4 top-6 right-8 w-[10%] flex flex-row justify-around">
-      {currentUrl.pathname.includes("main") && (
+      {currentUrl.pathname.includes("about") && (
         <div className="text-3xl sm:text-2xl">
           <Icon
             onClick={() => navigate("/")}
